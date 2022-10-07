ad281180b06d7f551f79fae40debeadcfb3b1e9e
Paco Hdez <francj.hdez@gmail.com>
2022-10-07 12:20:58 -0500

    6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints number from 0 to 10
+ * in a new line
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+
+
