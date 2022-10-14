aed6be7ec11de9f18b5c12a5eee21f24748a2836
Paco Hdez <francj.hdez@gmail.com>
2022-10-14 11:59:50 -0500

    The “Fizz-Buzz test”

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@
 
 int main(void)
 {
-	int i = 1 = 1 = 1 = 1 = 1 = 1 = 1 = 1 = 1;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	for (; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
