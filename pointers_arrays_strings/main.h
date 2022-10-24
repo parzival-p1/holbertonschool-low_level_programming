407b42613108757cf86620b9f0d0d1d0776845d4
Paco Hdez <francj.hdez@gmail.com>
2022-10-23 22:05:36 -0500

    Adding new func to the file main.h - C-More_pointers_arrays_and_strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,3 +9,31 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+
+/* C More pointers, arrays and strings */
+
+/* 0. strcat */
+char *_strcat(char *dest, char *src)
+
+/* 1. strnca */
+char *_strncat(char *dest, char *src, int n)	
+
+/* 2. strncpy  */i
+char *_strncpy(char *dest, char *src, int n)
+
+/* 3. strcmp */
+int _strcmp(char *s1, char *s2)
+
+/* 4. I am a kind of paranoid in reverse. */
+void reverse_array(int *a, int n)	
+
+/* 5. Always look up  */
+char *string_toupper(char *)i
+
+/* 6. Expect the best. Prepare for the worst */
+char *cap_string(char *)
+
+/* 7. Mozart composed his music not for the elite, but for everybody 
+ * Write a function that encodes a string into 1337.
+ */
+
