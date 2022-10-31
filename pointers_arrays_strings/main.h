506a4061b618c0358f0ddccf72eed657469bac68
Paco Hdez <francj.hdez@gmail.com>
2022-10-30 22:27:52 -0600

    updating main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,6 +10,10 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 
+
+/** *_* PROJECT 2 *_* **/
+
+
 /* C More pointers, arrays and strings */
 
 /* 0. strcat */
@@ -37,3 +41,32 @@ char *cap_string(char *);
  * Write a function that encodes a string into 1337.
  */
 
+
+/** *_* PROJECT 3 *_*  **/ 
+
+
+/* C - Even more pointers, arrays and strings */
+
+/* 0. Write a function that fills memory with a constant byte. */
+char *_memset(char *s, char b, unsigned int n);
+
+/* 1. Write a function that copies memory area */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* 2. Write a function that locates a character in a string. */
+char *_strchr(char *s, char c);
+
+/* 3.  Write a function that gets the length of a prefix substring. */
+unsigned int _strspn(char *s, char *accept);
+
+/* 4. Write a function that searches a string for any of a set of bytes. */
+char *_strpbrk(char *s, char *accept);
+
+/* 5. Write a function that locates a substring. */
+char *_strstr(char *haystack, char *needle);
+
+/* 6.  Write a function that prints the chessboard. */
+void print_chessboard(char (*a)[8]);
+
+/* 7. Write a function that prints the sum of the two diagonals of a square matrix of integers.  */
+void print_diagsums(int *a, int size);
