d8789ac27cb9521070723c76a3ee02f755013457
Paco Hdez <francj.hdez@gmail.com>
2022-11-03 11:03:20 -0600

    √

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * struct dog d: dog structure
+ *
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owne
+ */
+
+struct dog d
+{
+	char * name;
+	float age;
+	char * owner;
+}
+
