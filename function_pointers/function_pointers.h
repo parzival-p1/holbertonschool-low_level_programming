a52b88bae05a7edf05c906c9d68a4725dc29f881
Paco Hdez <francj.hdez@gmail.com>
2022-11-04 22:13:56 -0600

    uploading header file

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/* 0. Write a function that prints a name */
+void print_name(char *name, void (*f)(char *));
+
+/** 1.  Write a function that executes a function given
+ *  as a parameter on each element of an array.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* 2. Write a function that searches for an integer. */
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
