bd8bf7cd4fc3c76c3996597b8e636b279c317666
Paco Hdez <francj.hdez@gmail.com>
2022-11-08 19:40:28 -0600

    in C Write a function that prints a string in reverse,

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a str, in reverse.
+ *
+ * @s: ptr to the 1st element of a str of charachters
+ * return: Always 0 (success);
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+		_putchar(*s);
+	_print_rev_recursion(s + 1);
+}
+
