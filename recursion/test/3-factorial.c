858a2fba046f6075d8b3a02f8db05caa58368d6b
Paco Hdez <francj.hdez@gmail.com>
2022-11-13 13:00:13 -0600

    adding test dir

---
+++ recursion/test/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: any given number
+ *
+ * return: Always 0 (success)
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
+
