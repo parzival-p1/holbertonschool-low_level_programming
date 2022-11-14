e6efabd18a565596ee9f46247c7c2d09efa5e729
Paco Hdez <francj.hdez@gmail.com>
2022-11-14 15:03:21 -0600

    Write a program that prints all arguments it receives.

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	int i;
 
@@ -16,5 +16,6 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
+	return (0);
 }
 
