fd48fc87a031c867d7f58f11c9f535808ccd7b84
Paco Hdez <francj.hdez@gmail.com>
2022-11-19 01:20:37 -0600

    updating lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -20,24 +20,19 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
-/* Write a function that prints all the elements of
-   a list_t list. */
+/* Write a function that prints all the elements of a list_t list.*/
 size_t print_list(const list_t *h);
 
-/* 1. Write a function that returns the number of
-  elements in a linked list_t list. */
+/* 1. Write a function that returns the number of elements in a linked list_t list.*/
 size_t list_len(const list_t *h);
 
-/* 2. Write a function that adds a new node at the
-   beginning of a list_t list.*/
+/* 2. Write a function that adds a new node at the beginning of a list_t list.*/
 list_t *add_node(list_t **head, const char *str);
 
-/* 3. Write a function that adds a new node at
-   the end of a list_t list */
+/* 3. Write a function that adds a new node at the end of a list_t list.*/
 list_t *add_node_end(list_t **head, const char *str);
 
-/* 4. Write a function that frees a list_t list. */
+/* 4. Write a function that frees a list_t list.*/
 void free_list(list_t *head);
 
 /* putchar function */
