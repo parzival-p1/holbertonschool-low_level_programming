699f12bedb5cf417d687b91be31f26f3cd4850f9
Paco Hdez <francj.hdez@gmail.com>
2022-11-19 01:24:29 -0600

    updating lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -23,7 +23,7 @@ typedef struct list_s
 /* Write a function that prints all the elements of a list_t list.*/
 size_t print_list(const list_t *h);
 
-/*1 Write a function that returns the num of elements in a linked list_t list.*/
+/*1 Write a function that returns the num of elements in a linked list_t list*/
 size_t list_len(const list_t *h);
 
 /*2 Write a function that adds a new node at the beginning of a list_t list.*/
