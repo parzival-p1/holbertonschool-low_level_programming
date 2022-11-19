00cc35c2ec54096d414a00794b850bfcc0a7f570
Paco Hdez <francj.hdez@gmail.com>
2022-11-19 00:57:11 -0600

    updating lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -13,12 +13,12 @@ size_t list_len(const list_t *h);
 /* 2. Write a function that adds a new node at the beginning
  * of a list_t list.
  */
-list_t *add_node(list_t **head, const char *str);
+sigset_t *add_node(list_t **head, const char *str);
 
 /* 3. Write a function that adds a new node at the end
  * of a list_t list.
  */
-list_t *add_node_end(list_t **head, const char *str);
+sigset_t *add_node_end(list_t **head, const char *str);
 
 /* 4. Write a function that frees a list_t list. */
 void free_list(list_t *head);
