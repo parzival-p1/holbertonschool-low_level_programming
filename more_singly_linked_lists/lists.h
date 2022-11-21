cc15ea66ce27f8c27e2a7aaa31823af864406a2c
Paco Hdez <francj.hdez@gmail.com>
2022-11-21 14:37:45 -0600

    updating lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -47,7 +47,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 
 /* 9.1 Creates a node */
-list_t *create_node(int n);
+listint_t *create_node(int n);
 /* 9. Inserts a new node at a given position.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
