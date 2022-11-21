a3e8ddfc067c9b2fe2aa2f014862ed8085143686
Paco Hdez <francj.hdez@gmail.com>
2022-11-21 01:03:39 -0600

    updating lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -19,24 +19,16 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- *  0. Write a function that prints all the elements of
- *  a listint_t list.
- */
+/* 0. Write a function that prints all the elements of a listint_t list. */
 size_t print_listint(const listint_t *h);
 
-/**
- * 1.  Write a function that returns the number of
- * elements in a linked listint_t list.
- */
+/* 1.  Write a function that returns the number of elements in a linked listint_t list */
 size_t listint_len(const listint_t *h);
 
-/* 2. Write a function that adds a new node at
-   the beginning of a listint_t list. */
+/* 2. Write a function that adds a new node at the beginning of a listint_t list. */
 listint_t *add_nodeint(listint_t **head, const int n);
 
-/* 3. Write a function that adds a new node at
-   the end of a listint_t list. */
+/* 3. Write a function that adds a new node at the end of a listint_t list. */
 listint_t *add_nodeint_end(listint_t **head, const int n);
 
 /* 4. Write a function that frees a listint_t list. */
@@ -45,23 +37,18 @@ void free_listint(listint_t *head);
 /* 5. Write a function that frees a listint_t list. */
 void free_listint2(listint_t **head);
 
-/* 6. Write a function that deletes the head node of
-   a listint_t linked list, and returns the head node’s data (n). */
-int pop_listint(listint_t **head);
+/* 6. Write a function that deletes the head node of a listint_t linked list, and returns the head node’s data (n). */
 
-/* 7. Write a function that returns the nth node of
-   a listint_t linked list. */
+/* 7. Write a function that returns the nth node of  a listint_t linked list. */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 /* 8. Write a function that returns the sum of all the data (n) of a listint_t linked list. */
 int sum_listint(listint_t *head);
 
-/* 9. Write a function that inserts a new node at
-   a given position.*/
+/* 9. Write a function that inserts a new node at a given position.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
-/* Write a function that deletes the node at index
-   index of a listint_t linked list. */
+/* 10. Write a function that deletes the node at index index of a listint_t linked list. */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif /* _LISTS_H_ */
