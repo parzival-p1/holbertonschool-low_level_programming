58af7115eade91e367126364a03f6655ac337624
Paco Hdez <francj.hdez@gmail.com>
2022-11-21 00:57:44 -0600

    updating lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -19,13 +19,16 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** 0. Write a function that prints all the elements of
+/**
+ *  0. Write a function that prints all the elements of
  *  a listint_t list.
  */
 size_t print_listint(const listint_t *h);
 
-/* 1.  Write a function that returns the number of
-   elements in a linked listint_t list. */
+/**
+ * 1.  Write a function that returns the number of
+ * elements in a linked listint_t list.
+ */
 size_t listint_len(const listint_t *h);
 
 /* 2. Write a function that adds a new node at
