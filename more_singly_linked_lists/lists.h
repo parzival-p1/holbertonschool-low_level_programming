e727941c284c6f8e34c17c4fad2dbf4f520dc209
Paco Hdez <francj.hdez@gmail.com>
2022-11-20 21:30:08 -0600

    adding lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -42,7 +42,7 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 
 /* 6. Write a function that deletes the head node of
-   a listint_t linked list, and returns the head node’s data (n).  */
+   a listint_t linked list, and returns the head node’s data (n). */
 int pop_listint(listint_t **head);
 
 /* 7. Write a function that returns the nth node of
