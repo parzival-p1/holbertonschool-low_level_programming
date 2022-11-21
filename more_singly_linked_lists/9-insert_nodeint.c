89af72b560943a27ca4ed81485500523577e96c0
Paco Hdez <francj.hdez@gmail.com>
2022-11-21 14:39:22 -0600

    Write a function that inserts a new node at a given position.

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - adds a new node at the end of a list
+ * @head: pointer to the struct listint_t
+ * @idx: index of the list
+ * @n: integer value
+ * Return: the addres of the new element
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *temp = *head, *back = NULL;
+
+	unsigned int pos = -1, insert = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = create_node(n);
+
+	if (*head == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		pos++;
+
+		if (pos == idx)
+		{
+			if (back == NULL)
+			{
+				new->next = *head;
+				*head = new;
+			}
+			else
+			{
+				back->next = new;
+				new->next = temp;
+			}
+			insert++;
+		}
+		else
+		{
+			back = temp;
+			temp = temp->next;
+		}
+	}
+	if (!insert)
+		return (NULL);
+	return (*head);
+}
+
+/**
+ * create_node - creates a new node
+ * @n: the integer value to be returned
+ * Return: the new node
+ */
+
+listint_t *create_node(int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	return (new);
+}
+
