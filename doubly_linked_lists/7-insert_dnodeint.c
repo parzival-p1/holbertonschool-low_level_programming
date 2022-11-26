da21f3089ea78376d1283519499a12a0f21d4405
Paco Hdez <francj.hdez@gmail.com>
2022-11-26 13:28:21 -0600

    Write a function that inserts a new node at a given position.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,45 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - adds a new node at the end of a list
- * @head: pointer to the struct listint_t
+ * insert_dnodeint_at_index - adds a new node at the end of a list
+ * @h: pointer to the struct dlistint_t
  * @idx: index of the list
  * @n: integer value
- * Return: the addres of the new element
+ * Return: the addres of the new node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_n, *temp;
-
 	unsigned int pos = 0;
+
+	if (h == NULL)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	temp = *h;
 
-	while (temp)
-	{
-		if (pos == idx)
-		{
-			if (temp->next == NULL)
-				new_n = add_dnodeint_end(h, n);
-			else
-			{
-				new_n = malloc(sizeof(dlistint_t));
-				if (new_n == NULL)
-					return (NULL);
-				new_n->n = n;
-
-				temp->prev->next = new_n;
-				new_n->prev = temp->prev;
-				temp->prev  = new_n;
-				new_n->next = temp;
-			}
-		}
-		pos++;
+	while (pos++ < idx && temp)
 		temp = temp->next;
+
+	if (pos == idx && !temp)
+		return (add_dnodeint_end(h, n));
+	else
+	{
+		if (temp)
+		{
+			new_n = malloc(sizeof(dlistint_t));
+			if (new_n == NULL)
+				return (NULL);
+			new_n->n = n;
+			temp->prev->next = new_n;
+			new_n->prev = temp->prev;
+			temp->prev  = new_n;
+			new_n->next = temp;
+
+			return (new_n);
+		}
 	}
-	if (idx > pos)
-		return (NULL);
-	return (new_n);
+	return (NULL);
 }
 
