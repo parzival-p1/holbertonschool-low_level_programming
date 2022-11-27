fe6272453d00bdc203e66a9363f2301d510418e1
Paco Hdez <francj.hdez@gmail.com>
2022-11-27 10:52:17 -0600

    Updating mainn.h

--- file_io/main.h
+++ file_io/main.h
@@ -9,10 +9,17 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+/* Additional functions */
 int _putchar(char c);
 int _strlen(char *s);
+
+/* 0. reads a text file and prints it to the POSIX standard output */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1. creates a file */
 int create_file(const char *filename, char *text_content);
+
+/* 2. appends text at the end of a file */
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
