dfe92c988e6354cd967d31c80458e1f92e464323
Paco Hdez <francj.hdez@gmail.com>
2022-11-30 13:08:07 -0600

    updating main.h

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,26 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+int putchar(char c);
+int islower(int c);
+int isalpha(int c);
+int abs(int n);
+int isupper(int c);
+int isdigit(int c);
+int strlen(char *s);
+void puts(char *s);
+char *strcpy(char *dest, char *src);
+int atoi(char *s);
+char *strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int strcmp(char *s1, char *s2);
+char *memset(char *s, char b, unsigned int n);
+char *memcpy(char *dest, char *src, unsigned int n);
+char *strchr(char *s, char c);
+unsigned int strspn(char *s, char *accept);
+char *strpbrk(char *s, char *accept);
+char *strstr(char *haystack, char *needle);
 
 #endif
 
