2453abc34ef0b7874b62b816c1f01370a711dbbf
Paco Hdez <francj.hdez@gmail.com>
2022-12-03 13:12:02 -0600

    Write a program that adds positive numbers.

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,11 +1,39 @@
 #include "main.h"
+#include <string.h>
 /**
- *
- *
- *
- *
- *
- *
+ * main - Adds only positive numbers
+ * @argc: num of args passed to the program
+ * @argv: arr of strs
  */
 
+int main (int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	unsigned long j;
+
+	if (argc == 1)
+	{
+		printf("argc = %d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+					return (1);
+			}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("Result = %d\n", sum);
+
+	return (0);
+}
 
