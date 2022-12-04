d9e14f63d7309fcdfc1c3a82d8d4db36b74b7ffb
Paco Hdez <francj.hdez@gmail.com>
2022-12-03 22:11:40 -0600

    updating main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -4,15 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 0. allocates memory using malloc */
 void *malloc_checked(unsigned int b);
 
+/* 1. concatenates two strings */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+/* 2. allocates memory for an array, using malloc */
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/* 3. creates an array of integers */
 int *array_range(int min, int max);
 
-
-
-#endif
+#endif /* MAIN_H */
 
