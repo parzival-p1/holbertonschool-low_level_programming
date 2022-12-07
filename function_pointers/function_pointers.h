2aa65ce9b9b56e7065151c648adf0e00cf706781
Paco Hdez <francj.hdez@gmail.com>
2022-12-07 14:23:46 -0600

    updating main.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,9 +7,7 @@
 /* 0. Write a function that prints a name */
 void print_name(char *name, void (*f)(char *));
 
-/** 1.  Write a function that executes a function given
- *  as a parameter on each element of an array.
- */
+/* 1.  Write a function that executes a function given as a parameter on each element of an array */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* 2. Write a function that searches for an integer. */
