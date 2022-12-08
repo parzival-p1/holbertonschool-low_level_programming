231868f5aa6e44216f350f71c33cc6a505af00a2
Paco Hdez <francj.hdez@gmail.com>
2022-12-07 20:50:41 -0600

    updating main function

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (a, b)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(98);
-
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-		printf("%d\n", get_op_func(argv[2])(a, b));
-		return (0);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(a, b));
+	return (0);
 }
 
