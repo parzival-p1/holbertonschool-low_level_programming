a074a63e8bec8dfc9592b53b5c3091e6c686cd67
Paco Hdez <francj.hdez@gmail.com>
2022-12-07 20:36:16 -0600

    adding main functions

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * main - Program that performs simple operations
+ * @argc: number of arguments, passed.
+ * @argv: array of "string" pointers with the arguments
+ * Return: always 0 (succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (a, b)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		printf("%d\n", get_op_func(argv[2])(a, b));
+		return (0);
+	}
+}
+
