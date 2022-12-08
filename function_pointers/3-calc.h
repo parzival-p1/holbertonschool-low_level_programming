94e87bb5f0dbb97e231c0d00b8cdf9cb3b6081e8
Paco Hdez <francj.hdez@gmail.com>
2022-12-07 19:52:20 -0600

    adding header file

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,32 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t
+
+/* returns the sum of a and b */
+int op_add(int a, int b);
+
+/* returns the difference of a and b */
+int op_sub(int a, int b);
+
+/* returns the product of a and b */
+int op_mul(int a, int b);
+
+/* returns the result of the division of a by b */
+int op_div(int a, int b);
+
+/* returns the remainder of the division of a by b */
+op_mod(int a, int b);
+
+#endif /*  _CALC_H_ */
+
