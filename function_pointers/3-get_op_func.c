69c34ab2717141ff4c23ea5c5f975249fe8d53e6
Paco Hdez <francj.hdez@gmail.com>
2022-12-09 15:13:37 -0600

    fixing minor bugs

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - selects the operation to perform
  * @s: operation to be perform
@@ -19,9 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
+	{	
+		if (strcmp(s, ops[i].op) == 0)
+			break;
 		i++;
-
+	}	
 	return (ops[i].f);
 }
 
